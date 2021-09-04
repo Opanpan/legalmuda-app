b14dbffbec136c7c0c50c250a7a626f87d55c0ff
Opanpan <fanalriansyah@gmail.com>
2021-09-05 02:17:04 +0700

    Add function when mouse enter and leave header item

--- pages/components/Header.jsx
+++ pages/components/Header.jsx
@@ -12,18 +12,49 @@ export default function Header(props) {
     setIsClick(`${styles.navbarClicked}`);
   }, []);
 
+  const hoverHeader = (e) => {
+    console.log(e.target.id);
+    if (e.target.id !== props.pageName) {
+      setIsClick("");
+    }
+  };
+
+  const unHoveHeader = () => {
+    setGetId(props.pageName);
+    setIsClick(`${styles.navbarClicked}`);
+  };
+
   return (
     <Container fluid className={`p-0 ${styles.navbarPosition}`}>
       <nav className="navbar navbar-expand-lg">
         <div className={`${styles.navbarRRQ} ${styles.red} `}>
           <Link href="/">
-            <a className={getId === "/" ? isClick : ""}>Home</a>
+            <a
+              id="/"
+              onMouseEnter={hoverHeader}
+              onMouseLeave={unHoveHeader}
+              className={getId === "/" ? isClick : ""}
+            >
+              Home
+            </a>
           </Link>
           <Link href="/artikel">
-            <a className={getId === "/artikel" ? isClick : ""}>Artikel</a>
+            <a
+              id="/artikel"
+              onMouseEnter={hoverHeader}
+              onMouseLeave={unHoveHeader}
+              className={getId === "/artikel" ? isClick : ""}
+            >
+              Artikel
+            </a>
           </Link>
           <Link href="/legalitas">
-            <a className={getId === "/legalitas" ? isClick : ""}>
+            <a
+              id="/legalitas"
+              onMouseEnter={hoverHeader}
+              onMouseLeave={unHoveHeader}
+              className={getId === "/legalitas" ? isClick : ""}
+            >
               Legal Muda Services
             </a>
           </Link>
