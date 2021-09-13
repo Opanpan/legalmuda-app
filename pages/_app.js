ff7ab294500bafc2fadc73a38dcd276a6cfc8fb6
Opanpan <fanalriansyah@gmail.com>
2021-09-13 17:43:39 +0700

    Add custom style for font

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/global.scss";
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />
-  }
-  
+  return <Component {...pageProps} />;
+}
