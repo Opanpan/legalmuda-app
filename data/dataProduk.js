59cf624528fd94c93f1da26a9a8dd681be30650d
Opanpan <fanalriansyah@gmail.com>
2021-09-13 17:44:22 +0700

    Create dataProduk json

---
+++ data/dataProduk.js
@@ -0,0 +1,26 @@
+import Legalitas from "../assets/img/Legalitas 1.svg";
+import VirtualOffice from "../assets/img/Office 1.svg";
+import Branding from "../assets/img/Branding Usaha 1.svg";
+
+const dataProduk = [
+  {
+    id: 1,
+    tittle: "Legalitas Usaha",
+    img: <Legalitas />,
+    link: "/legalitas",
+  },
+  {
+    id: 2,
+    tittle: "Virtual Office",
+    img: <VirtualOffice />,
+    link: "/virtual-office",
+  },
+  {
+    id: 3,
+    tittle: "Branding Usaha",
+    img: <Branding />,
+    link: "/branding-usaha",
+  },
+];
+
+export default dataProduk;
